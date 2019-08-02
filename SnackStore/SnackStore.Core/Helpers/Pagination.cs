76e036b4ee3c9338ba8df90da2960b1365958b7b
Elmer Mauricio Chacon Osorio <treechacon@crecer.com.sv>
2019-08-02 13:59:33 -0600

    Product repository

---
+++ SnackStore/SnackStore.Core/Helpers/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnackStore.Core.Helpers
+{
+    public class Pagination
+    {
+        public int Number { get; set; }
+        public int PageSize { get; set; }
+        public string Sort { get; set; }
+        public string Order { get; set; }
+    }
+}
---
+++ SnackStore/SnackStore.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using SnackStore.Core.Helpers;
+using SnackStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackStore.Core.Interfaces
+{
+    public interface IProductRepository: IRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<IEnumerable<Product>> GetAllPaginated(Pagination pagination);
+        Task<Product> GetById(int id);
+        Task<Product> GetByName(string name);
+        Task EditProduct(Product product);
+        Task AddProduct(Product product);
+        Task RemoveProduct(Product product);
+    }
+}
